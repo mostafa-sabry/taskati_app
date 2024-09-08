4af0f08d137f257a938d418fe5048493a0489f52
mostafa-sabry <engmostafasabryhassan@gmail.com>
2024-09-07 19:07:47 -0700

    create type adabter for task

--- lib/models/task_model.dart
+++ lib/models/task_model.dart
@@ -1,8 +1,17 @@
-class TaskModel {
+import 'package:hive/hive.dart';
+
+
+@HiveType(typeId: 0)
+class TaskModel extends HiveObject {
+  @HiveField(0)
   final String title;
+  @HiveField(1)
   final String subTitle;
+  @HiveField(2)
   final String date;
+  @HiveField(3)
   final int color;
+  
 
   TaskModel({
     required this.title,
@@ -10,4 +19,5 @@ class TaskModel {
     required this.date,
     required this.color,
   });
+
 }
