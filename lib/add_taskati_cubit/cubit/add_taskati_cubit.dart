dc30d7903833656eac88cfcd4aff5cd5c6b3ae75
mostafa-sabry <engmostafasabryhassan@gmail.com>
2024-09-22 07:21:04 +0300

    Update add_taskati_cubit.dart
    create add task method

--- lib/add_taskati_cubit/cubit/add_taskati_cubit.dart
+++ lib/add_taskati_cubit/cubit/add_taskati_cubit.dart
@@ -1,6 +1,6 @@
-import 'package:bloc/bloc.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:hive_flutter/hive_flutter.dart';
-import 'package:meta/meta.dart';
 import 'package:taskati_app/models/task_model.dart';
 
 part 'add_taskati_state.dart';
@@ -8,7 +8,15 @@ part 'add_taskati_state.dart';
 class AddTaskatiCubit extends Cubit<AddTaskatiState> {
   AddTaskatiCubit() : super(AddTaskatiInitial());
 
-  addTask(TaskModel task) {
-    Hive.box('taskati');
+  addTask(TaskModel task) async {
+    emit(AddTaskatiLoading());
+    try {
+      var taskBox = Hive.box<TaskModel>('taskati');
+
+      emit(AddTaskatiSuccess());
+      await taskBox.add(task);
+    } catch (e) {
+      emit(AddTaskatiError(errorMessage: e.toString()));
+    }
   }
 }
