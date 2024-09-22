601632322deeaced1b3080190f808bac7bdd0cda
mostafa-sabry <engmostafasabryhassan@gmail.com>
2024-09-22 07:10:46 +0300

    Update add_taskati_cubit.dart

--- lib/add_taskati_cubit/cubit/add_taskati_cubit.dart
+++ lib/add_taskati_cubit/cubit/add_taskati_cubit.dart
@@ -1,8 +1,14 @@
 import 'package:bloc/bloc.dart';
+import 'package:hive_flutter/hive_flutter.dart';
 import 'package:meta/meta.dart';
+import 'package:taskati_app/models/task_model.dart';
 
 part 'add_taskati_state.dart';
 
 class AddTaskatiCubit extends Cubit<AddTaskatiState> {
   AddTaskatiCubit() : super(AddTaskatiInitial());
+
+  addTask(TaskModel task) {
+    Hive.box('taskati');
+  }
 }
